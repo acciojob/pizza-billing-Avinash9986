7924f260cb3d958c9d0ff11887cdb4b190d5bb41
Your Name <avinashpsahukar@gmail.com>
2023-08-31 16:25:00 +0530

    attempt no 3

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -3,7 +3,15 @@ package com.driver;
 public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
+
         super(isVeg);
         // your code goes here
+
+
+
+
+
+
     }
+
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -9,6 +9,14 @@ public class Pizza {
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if (isVeg){
+            bill = "Base Price Of The Pizza: 300 \n" ;
+            price += 300 ;
+        }
+        else {
+            bill = "Base Price Of The Pizza: 400 \n" ;
+            price += 400 ;
+        }
     }
 
     public int getPrice(){
@@ -17,18 +25,31 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        price += 80 ;
+        bill += "Extra Cheese Added: 80 \n" ;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg){
+            price += 70 ;
+            bill += "Extra Toppings Added: 70 \n" ;
+        }
+        else{
+            price += 120 ;
+            bill += "Extra Toppings Added: 120 \n " ;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        price += 20 ;
+        bill += "Paperbag Added: 20 \n" ;
     }
 
     public String getBill(){
         // your code goes here
+        bill +=  "Total Price: " + price ;
         return this.bill;
     }
 }
